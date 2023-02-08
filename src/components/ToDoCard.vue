891ca868326f0f9b2d49f30a63c11986ea60a504
Barabas Balazs <barabas.balazska@gmail.com>
2023-02-08 13:05:41 +0200

    ITL - 24 Make svg animated

--- src/components/ToDoCard.vue
+++ src/components/ToDoCard.vue
@@ -14,10 +14,17 @@
           <p class="mx-5">{{ item.priority }}</p>
         </div>
         <div
-          class="p-2 rounded-full h-fit border-2 border-black lg:hidden mr-5"
+          class="flex justify-center items-center p-2 rounded-full h-fit border-2 border-black lg:hidden mr-5 relative"
           :class="colorOfCheckCircle"
           @click.stop="toggleResolvedStatus"
-        ></div>
+        >
+          <svg v-if="isResolved" 
+            class="animated-check absolute z-10 w-8 h-8 pb-1 pl-1" 
+            viewBox="0 0 24 24"
+          >
+            <path d="M4.1 12.7L9 17.6 20.4 4.1" fill="none" />
+          </svg>
+        </div>
         <div class="flex flex-col text-start lg:hidden">
           <p class="text-lg font-medium">{{ item.title }}</p>
           <p class="text-xs text-grey opacity-30">
@@ -39,12 +46,12 @@
         <div class="p-3 rounded-full h-fit border-4 flex justify-center items-center"
           :class="colorOfCheckCircle"
           @click.stop="toggleResolvedStatus">
-          <Transition>
-            <img v-if="isResolved"
-            src="../assets/icons/Vectorcheck.svg"
-            class="absolute z-10 bottom-2 w-10 h-8 click:transition"
-            />
-          </Transition>  
+          <svg v-if="isResolved" 
+            class="animated-check absolute z-10 w-16 h-16 pl-2 pb-4" 
+            viewBox="0 0 24 24"
+          >
+            <path d="M4.1 12.7L9 17.6 20.4 4.1" fill="none" />
+          </svg>
         </div>
       </div>
       <div class="lg:hidden">
@@ -84,15 +91,20 @@
 </script>
 
 <style>
-  .v-enter-active,
-  .v-leave-active {
-    transition: opacity 0.5s ease;
-    transition: width 0.5s ease;
-  }
 
-  .v-enter-from,
-  .v-leave-to {
-    width: 0;
-    opacity: 0;
+.animated-check path {
+  fill: none;
+  stroke: #4fda9b;
+  stroke-width: 3;
+  stroke-dasharray: 23;
+  stroke-dashoffset: 20;
+  animation: draw 0.25s linear forwards;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+@keyframes draw {
+  to {
+    stroke-dashoffset: 2;
   }
+}
 </style>
